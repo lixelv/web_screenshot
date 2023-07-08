8afedcfd55d2b31853a015b7b1423c5fd4d90df1
lixel_v <simeongfremenko@gmail.com>
2023-07-08 16:07:13 +0300

    Update

--- parse.py
+++ parse.py
@@ -1,29 +1,4 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import time
-import os
-
-def create_folder_if_not_exists(folder_name):
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
-
-width = int(open('config.txt', 'r').readline())
-
-def capture_full_page_screenshot(driver, url, name=None, width=1920):
-    try:
-        start_time = time.perf_counter()
-        driver.get(url)
-        title = driver.title
-        name = title if name == None else name
-        scroll_height = driver.execute_script('return document.documentElement.scrollHeight')
-        driver.set_window_size(width, scroll_height)
-        create_folder_if_not_exists("image")
-        screenshot = driver.save_screenshot('image/'+name+'.png')
-        print(f"Скриншот сохранен в файле '{name}.png'")
-
-    finally:
-        end_time = time.perf_counter()
-        return (title, end_time - start_time)
+from url import *
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
---
+++ url.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+import os
+
+
+def create_folder_if_not_exists(folder_name):
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+width = int(open('config.txt', 'r').readline())
+
+
+def find_max_numbered_file(directory_path):
+    only_numbers = []
+    directory_path = os.path.join(os.getcwd(),directory_path)
+
+    for filename in os.listdir(directory_path):
+        filepath = os.path.join(directory_path, filename)
+        if os.path.isfile(filepath):
+            name_without_extension = os.path.splitext(filename)[0]
+            if name_without_extension.isdigit():
+                only_numbers.append(int(name_without_extension))
+
+    if not only_numbers:
+        return 1
+    else:
+        return max(only_numbers)+1
+
+
+def capture_full_page_screenshot(driver, url, path='image', width=1920):
+    url = url if url[:4] in ['file', 'http'] else 'http://'+url
+    try:
+        start_time = time.perf_counter()
+        driver.get(url)
+        title = driver.title
+        scroll_height = driver.execute_script('return document.documentElement.scrollHeight')
+        driver.set_window_size(width, scroll_height)
+        screenshot = driver.save_screenshot(f'{path}/{find_max_numbered_file(path)}.png')
+
+    finally:
+        end_time = time.perf_counter()
+        return (title, end_time - start_time)
